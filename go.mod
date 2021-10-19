850f46b68f99dd780d22ed629b0d963bee19f868
落心 <luoxin.ttt@gmail.com>
2021-10-19 13:37:58 +0800

    输出美化
    Signed-off-by: 落心 <luoxin.ttt@gmail.com>

--- go.mod
+++ go.mod
@@ -10,9 +10,9 @@ require (
 	github.com/darabuchi/enputi v0.0.6
 	github.com/eddieivan01/nic v0.3.1
 	github.com/elliotchance/pie v1.39.0
-	github.com/gofiber/fiber/v2 v2.20.2
 	github.com/gookit/color v1.5.0
 	github.com/ncruces/go-dns v1.1.0
+	github.com/pterm/pterm v0.12.31
 	github.com/rogpeppe/go-internal v1.8.0
 	github.com/sirupsen/logrus v1.8.1
 )
@@ -21,15 +21,19 @@ require (
 	github.com/Ferluci/fast-realip v1.0.0 // indirect
 	github.com/alexflint/go-scalar v1.1.0 // indirect
 	github.com/andybalholm/brotli v1.0.3 // indirect
+	github.com/atomicgo/cursor v0.0.1 // indirect
 	github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394 // indirect
 	github.com/btcsuite/btcd v0.22.0-beta // indirect
 	github.com/ethereum/go-ethereum v1.10.10 // indirect
+	github.com/gofiber/fiber/v2 v2.20.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/mcuadros/go-defaults v1.2.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.31.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
@@ -37,4 +41,5 @@ require (
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
 	golang.org/x/sys v0.0.0-20211015200801-69063c4bb744 // indirect
+	golang.org/x/term v0.0.0-20210916214954-140adaaadfaf // indirect
 )
--- modclean/main.go
+++ modclean/main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"bytes"
-	log "github.com/sirupsen/logrus"
+	"fmt"
+	"github.com/pterm/pterm"
 	"io/fs"
 	"os"
 	"os/exec"
@@ -14,6 +15,7 @@ import (
 type FileInfo struct {
 	ModTime  time.Time
 	FilePath string
+	Size     int64
 }
 
 func main() {
@@ -25,14 +27,15 @@ func main() {
 
 	err := cmd.Run()
 	if err != nil {
-		log.Errorf("err:%v", err)
+		pterm.Error.Sprintf("err:%v", err)
 		return
 	}
 
 	modRoot := strings.TrimSuffix(filepath.ToSlash(strings.TrimSuffix(out.String(), "\n")), "/") + "/"
 
-	log.Infof("go mod cache is %v", modRoot)
+	pterm.Success.Printfln("go mod cache is %v", modRoot)
 
+	bar, _ := pterm.DefaultSpinner.WithText("Scanning go.mod...").Start()
 	m := map[string][]FileInfo{}
 	err = filepath.Walk(modRoot, func(path string, info fs.FileInfo, err error) error {
 		if info == nil {
@@ -40,59 +43,92 @@ func main() {
 		}
 
 		if !info.IsDir() {
+			bar.UpdateText(pterm.FgLightMagenta.Sprintf("%v is not dir,skip", info.Name()))
 			return nil
 		}
 
 		if !strings.Contains(info.Name(), "@") {
+			bar.UpdateText(pterm.FgLightMagenta.Sprintf("%v is not go.mod path,skip", info.Name()))
 			return nil
 		}
 
 		modName := strings.Split(info.Name(), "@")[0]
 		if modName == "" {
+			bar.UpdateText(pterm.FgLightMagenta.Sprintf("%v is not go.mod path,skip", info.Name()))
 			return nil
 		}
 
 		m[modName] = append(m[modName], FileInfo{
 			ModTime:  info.ModTime(),
 			FilePath: path,
+			Size:     info.Size(),
 		})
-		log.Infof("found mod %s", info.Name())
+
+		bar.UpdateText(pterm.FgLightCyan.Sprintf("found go.mod path %s", info.Name()))
 
 		return nil
 	})
 	if err != nil {
-		log.Errorf("err:%v", err)
+		bar.FailPrinter.Printfln("err:%v", err)
 		return
 	}
+	bar.Success("scanned go.mod")
 
-	var removeCount uint32
-	for _, values := range m {
-		latestAt := time.Time{}
-		latestPath := ""
+	var allRemoveCount uint32
+	var allRemoveSize int64
+	bar, _ = pterm.DefaultSpinner.WithText("Scanning go.mod...").Start()
+	for key, values := range m {
+		lastVal := FileInfo{}
+
+		var removeCount uint32
+		var removeSize int64
+
+		remove := func(val FileInfo) {
+			bar.UpdateText(pterm.FgYellow.Sprintf("will remove %s", val.FilePath))
+			err = os.RemoveAll(val.FilePath)
+			if err != nil {
+				pterm.Error.Printfln("err:%v", err)
+				return
+			}
+
+			allRemoveCount++
+			allRemoveSize += val.Size
+
+			removeCount++
+			removeSize += val.Size
+		}
 
 		for _, val := range values {
-			if latestAt.Sub(val.ModTime) < 0 {
-				if latestPath != "" {
-					log.Warnf("will remove %s", latestPath)
-					removeCount++
-					err = os.RemoveAll(latestPath)
-					if err != nil {
-						log.Errorf("err:%v", err)
-					}
+			if lastVal.ModTime.Sub(val.ModTime) < 0 {
+				if lastVal.FilePath != "" {
+					remove(lastVal)
 				}
 
-				latestAt = val.ModTime
-				latestPath = val.FilePath
+				lastVal = val
 			} else {
-				log.Warnf("will remove %s", val.FilePath)
-				removeCount++
-				err = os.RemoveAll(val.FilePath)
-				if err != nil {
-					log.Errorf("err:%v", err)
-				}
+				remove(val)
 			}
 		}
+
+		bar.Success(pterm.FgLightGreen.Sprintf("%v clean %v version size %v", key, removeCount, formatFileSize(removeSize)))
 	}
 
-	log.Infof("find %v pkg, %v version is removed", len(m), removeCount)
+	bar.Success(pterm.FgLightGreen.Sprintf("find %v pkg, %v version is removed siz %v", len(m), allRemoveCount, formatFileSize(allRemoveSize)))
+}
+
+func formatFileSize(fileSize int64) (size string) {
+	if fileSize < 1024 {
+		//return strconv.FormatInt(fileSize, 10) + "B"
+		return fmt.Sprintf("%.2fB", float64(fileSize)/float64(1))
+	} else if fileSize < (1024 * 1024) {
+		return fmt.Sprintf("%.2fKB", float64(fileSize)/float64(1024))
+	} else if fileSize < (1024 * 1024 * 1024) {
+		return fmt.Sprintf("%.2fMB", float64(fileSize)/float64(1024*1024))
+	} else if fileSize < (1024 * 1024 * 1024 * 1024) {
+		return fmt.Sprintf("%.2fGB", float64(fileSize)/float64(1024*1024*1024))
+	} else if fileSize < (1024 * 1024 * 1024 * 1024 * 1024) {
+		return fmt.Sprintf("%.2fTB", float64(fileSize)/float64(1024*1024*1024*1024))
+	} else { //if fileSize < (1024 * 1024 * 1024 * 1024 * 1024 * 1024)
+		return fmt.Sprintf("%.2fEB", float64(fileSize)/float64(1024*1024*1024*1024*1024))
+	}
 }
